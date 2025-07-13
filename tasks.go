49df0157fd2de1941889d0ef8fc2eefe3800dcfb
nathy-cpu <degeadavid042@gmail.com>
2025-07-13 20:43:11 +0300

    Implement api for handling tasks

--- tasks.go
+++ tasks.go
@@ -1,7 +1,80 @@
-package main
+package tasks
 
-import "fmt"
+import (
+	"container/list"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
 
-func main() {
-	fmt.Println("a ...any")
+type taskItem struct {
+	Description string
+	Done bool
+	CreatedAt time.Time
+	DoneAt time.Time
+}
+
+type List []taskItem
+
+func (self *List) Add(task string) {
+	item := taskItem {
+		Description: task,
+		Done: false,
+		CreatedAt: time.Now(),
+		DoneAt: time.Time{},
+	}
+
+	*self = append(*self, item)
+}
+
+func (self *List) Complete(index int) error {
+	list := *self
+	if index <= 0 || index > len(list) {
+		return fmt.Errorf("Task item %d does not exist!", index)
+	}
+
+	list[index - 1].Done = true
+	list[index - 1].DoneAt = time.Now()
+
+	return nil
+}
+
+func (self *List) Delete(index int) error {
+	list := *self
+	if index <= 0 || index > len(list) {
+		return fmt.Errorf("Task item %d does not exist!", index)
+	}
+
+	*self = append(list[:index - 1], list[index:]...)
+
+	return nil
+}
+
+func (self *List) Save(filename string) error {
+	data, err := json.Marshal(self)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, data, 0644)
+}
+
+func (self *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		} else {
+			return err
+		}
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, self)
 }
